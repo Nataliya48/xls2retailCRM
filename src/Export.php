d7ee0c25da1f0f74e25d17ca62b73546cca67b16
nata <korovina-nataliya@mail.ru>
2019-04-02 11:58:58 +0300

    commit

--- src/Export.php
+++ src/Export.php
@@ -1,4 +1,18 @@
 <?php
 
-require_once '../vendor/autoload.php';
+class Export
+{
+    //В конструкторе проверить корректность открываемого файла, его существование и права
+    public function __construct()
+    {
+        if (is_uploaded_file($_FILES['event']['tmp_name'])) { // Определяет, был ли файл загружен при помощи HTTP POST
+            file_get_contents($_FILES['event']['tmp_name']); // если да, то считать информацию с него
+        } else {
+            throw new Exception('Access denied'); // если файл загружен левым путем отказать в доступе
+        }
 
+        if (mb_detect_encoding(file_get_contents($_FILES['event']['tmp_name'])) !== 'UTF-8'){
+            throw new Exception('Incorrect encoding. Use UTF-8'); // некорректная кодировка
+        }
+    }
+}
---
+++ src/template.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+
+</head>
+<body>
+
+<form enctype="multipart/form-data" method="post">
+    <p>Загрузите файл</p>
+    <p><input type="file" name="file" multiple accept="text/plain,application/excel,application/vnd.ms-excel,application/x-excel,application/x-msexcel">
+        <input type="submit" value="Отправить"></p>
+</form>
+
+
+
+</body>
