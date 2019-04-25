abf64f2356a20198e718fa7ecd6f4e565638845b
nata <korovina-nataliya@mail.ru>
2019-04-25 12:15:11 +0300

    edit css

--- src/templateLoadFile.php
+++ src/templateLoadFile.php
@@ -1,14 +1,17 @@
 <html>
 <head>
-    <link rel="stylesheet" href="style.css" type="text/css">
-    <title align="center">Export2CRM</title>
+    <link href="style.css" type="text/css" rel="stylesheet"/>
+    <title>Import2CRM</title>
 </head>
 <body>
 
-<h1 class="header-title">Export2CRM</h1>
+<h1 class="header-title">Import2CRM</h1>
+<p class="text">Импорт заказов и клиентов из файлов в формате Microsoft Excel либо CSV в retailCRM</p>
+
 <div>
-    <form enctype="multipart/form-data" method="post">
-        <p>Загрузите файл</p>
+    <form enctype="multipart/form-data" method="post" class="form">
+        <p class="load">Загрузите файл в формате *.xls(x) или *.csv</p>
+        <p class="load">Файл в формате CSV должен иметь кодировку utf8 с разделителем колонок ";" (точка с запятой).</p>
         <p><input type="file" name="file" multiple
                   accept="text/plain,application/excel,application/vnd.ms-excel,application/x-excel,application/x-msexcel">
             <input type="submit" value="Отправить"></p>
